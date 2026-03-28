801337631cd1900cc7ee99e5aa32e2bcb2aa77fe
akhrasnadim-beep <akhras.nadim@gmail.com>
2026-03-28 11:10:59 +0200

    Create __manifest__.py

---
+++ fiscalnet_pos/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "FiscalNet POS",
+    "version": "19.0.1.0.0",
+    "depends": ["point_of_sale"],
+    "data": [],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
